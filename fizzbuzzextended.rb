9a5704637898ef76455430e9880b93b8c6553e50
Ivan Storck <ivanoats@gmail.com>
2013-11-25 10:48:41 -0800

    modifications from class

--- fizzbuzzextended.rb
+++ fizzbuzzextended.rb
@@ -6,26 +6,24 @@
 
 
 class Fizzbuzz
+
+  BUZZERS = { 3 => "fizz", 5 => "buzz", 7 => "sivv", 10 => "hello" }
+
   attr_reader :numbers_avail, :nums
-  def initialize(options = {}, nums)
+  def initialize(nums = 100, options = BUZZERS )
     @numbers_avail = options
     @nums = nums
   end
 
   def evaluate
     (1..nums).each do |i|
-      match = false
+      output = nil
       numbers_avail.each do |k, v|
         if i % k == 0
-          match = true
-          print v
+          output = output.to_s + v
         end
       end
-      if !match
-        puts i
-      else
-        puts "\n"
-      end
+      puts (output || i)
     end
   end
 end
